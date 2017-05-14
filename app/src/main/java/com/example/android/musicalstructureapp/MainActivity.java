575b5ac4ecb5ae620a21671b5c8c6e6a79bee7c9
DVfr <dvremix@gmail.com>
2017-05-14 09:45:11 +0200

    Initial Commit

--- app/src/main/java/com/example/android/musicalstructureapp/MainActivity.java
+++ app/src/main/java/com/example/android/musicalstructureapp/MainActivity.java
@@ -1,7 +1,10 @@
 package com.example.android.musicalstructureapp;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,19 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+// Find the View that shows the ArtistList category
+        TextView artistList = (TextView) findViewById(R.id.artistListTextview);
+
+        artistList.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent playIntent = new Intent(MainActivity.this, ArtistListActivity.class);
+                startActivity(playIntent);
+
+            }
+
+        });
     }
 }
